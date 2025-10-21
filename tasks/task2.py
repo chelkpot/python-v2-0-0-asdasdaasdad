8ab5c413ca0732365f280819d896224a0ca3a477
asdasdaasdad <asdassdsdadda@gmail.com>
2025-10-21 14:45:35 +0500

    dz

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,10 +2,14 @@
 
 def solve():
 # Ниже пишите решение задачи
-
+    X, Y, Z = map(int,input().split())
+    price_pencil = 3
+    price_pen = price_pencil + 2
+    price_marker = price_pen + 7
+    total = X * price_pencil + Y * price_pen + Z * price_marker
+    print(total)
    
 
-   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
