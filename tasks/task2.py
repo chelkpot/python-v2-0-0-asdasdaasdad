05b1033fa4282f6113b5bc8993d2f99150f1043d
asdasdaasdad <asdassdsdadda@gmail.com>
2025-10-21 14:58:55 +0500

    dz

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,10 +3,10 @@
 def solve():
 # Ниже пишите решение задачи
     X, Y, Z = map(int,input().split())
-    price_pencil = 3
-    price_pen = price_pencil + 2
-    price_marker = price_pen + 7
-    total = X * price_pencil + Y * price_pen + Z * price_marker
+    pencil = 3
+    pen = pencil + 2
+    marker = pen + 7
+    total = X * pencil + Y * pen + Z * marker
     print(total)
 
     
